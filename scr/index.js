091ebfee2bba9f4de7ab7c2957552e0240e4839f
DESKTOP-QDHB8U5\feder <federicovongromann@gmail.com>
2019-02-22 18:55:17 -0300

    Add Dispatch And excersice file

--- scr/index.js
+++ scr/index.js
@@ -1,15 +1,6 @@
-function todos (state = [], action) {
-  
-  if (action.type === 'ADD_TODO') {
-    return state.concat([action.todo])
-  }
+// Library Code
 
-  return state
-}
-
-
-
-function createStore () {
+function createStore (reducer) {
   // The store should have four parts
   // 1. The state
   // 2. Get the state.
@@ -28,8 +19,42 @@ function createStore () {
     }
   }
 
+  const dispatch = (action) => {
+    state = reducer(state, action)
+    listeners.forEach((listener) => listener())
+  }
+
   return {
     getState,
-    subscribe
+    subscribe,
+    dispatch,
   }
 }
+
+
+
+// App Code
+// --------------------------------------------------------------------
+function todos (state = [], action) {
+  
+  if (action.type === 'ADD_TODO') {
+    return state.concat([action.todo])
+  }
+
+  return state
+}
+
+const store = createStore(todos)
+
+store.subscribe(() => {
+  console.log('The new state is: ', store.getState())
+})
+
+store.dispatch({
+  type: 'ADD_TODO',
+  todo: {
+    id: 0,
+    name: 'Learn Redux',
+    complete: false
+  }
+})
