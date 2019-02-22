f6ccffbd1acf7f5dd0ca94bcf76388221639d9b7
DESKTOP-QDHB8U5\feder <federicovongromann@gmail.com>
2019-02-22 14:53:41 -0300

    add subscribe function

--- scr/index.js
+++ scr/index.js
@@ -1,17 +1,24 @@
-function CreateSotre () {
+function createStore () {
+  // The store should have four parts
+  // 1. The state
+  // 2. Get the state.
+  // 3. Listen to changes on the state.
+  // 4. Update the state
 
-	// The Store Should Have four Parts.
-	// 1. The state.
-	// 2. Get the state.
-	// 3. Listen to changes on the state.
-	// 4. Update the state
-	let state
+  let state
+  let listeners = []
 
-	const getState = () => state
+  const getState = () => state
 
+  const subscribe = (listener) => {
+    listeners.push(listener)
+    return () => {
+      listeners = listeners.filter((l) => l !== listener)
+    }
+  }
 
-	return {
-		getState
-	}
-
+  return {
+    getState,
+    subscribe
+  }
 }
